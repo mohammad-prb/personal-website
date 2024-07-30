6cf47e04b50575a8b736eb8c88b936991a6fc215
Mohammad <pspipm27@gmail.com>
2024-07-30 19:20:10 +0330

    menu & content

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+$mobile = "09158955030";
+$email = "pspipm27@gmail.com";
+?>
 <!DOCTYPE html>
 <html lang="fa-ir">
 <head>
@@ -7,16 +11,132 @@
     <meta name="keywords" content="محمد پوربهزاد,محمد,پوربهزاد,پور بهزاد,محمد پور بهزاد,برنامه نویس,برنامه نویس وب,developer,web developer,frontend,backend,فرانت اند,بک اند,فول استک,mohammad,pourbehzad,mohammad pourbehzad,full stack"/>
     <meta name="description" content="برنامه نویس Full stack با تجربه، علاقه مند به پروژه ها و کارهای تیمی"/>
     <meta name="author" content="محمد پوربهزاد"/>
-    <link rel="shortcut icon" href="mohammad.png"/>
+    <link rel="shortcut icon" href="img/mohammad.png"/>
     <link rel="stylesheet" href="css/theme/light.css?v=1.0.0"/>
     <link rel="stylesheet" href="css/style.css?v=1.0.0"/>
 </head>
 <body dir="rtl">
     <div class="menu">
-
+        <img class="mainImage" src="img/main.jpg" alt="محمد پوربهزاد"/>
+        <h1 class="name">محمد پوربهزاد</h1>
+        <h2 class="title">برنامه نویس Full stack</h2>
+        <address class="info">
+            <div class="infoItem">
+                <span class="icon"></span>
+                <span class="text">تاریخ تولد: 1381 (22 سال)</span>
+            </div>
+            <div class="infoItem">
+                <span class="icon"></span>
+                <span class="text"><?php echo $mobile; ?></span>
+            </div>
+            <div class="infoItem">
+                <span class="icon"></span>
+                <span class="text"><?php echo $email; ?></span>
+            </div>
+            <div class="infoItem">
+                <span class="icon"></span>
+                <span class="text">خراسان رضوی - مشهد</span>
+            </div>
+            <div class="socials">
+                <a class="socialItem" href="tel:<?php echo $mobile; ?>" title="Mobile"></a>
+                <a class="socialItem" href="mailto:<?php echo $email; ?>" title="Email"></a>
+                <a class="socialItem" href="https://linkedin.com/in/mohammad-pourbehzad" title="LinkedIn" target="_blank"></a>
+                <a class="socialItem" href="https://github.com/mohammad-prb" title="Github" target="_blank"></a>
+                <a class="socialItem" href="https://t.me/Mohammad_prb" title="Telegram" target="_blank"></a>
+                <a class="socialItem" href="https://instagram.com/mammad.prb" title="Instagram" target="_blank"></a>
+                <a class="socialItem" href="https://wa.me/<?php echo $mobile; ?>" title="WhatsApp" target="_blank"></a>
+            </div>
+        </address>
     </div>
     <div class="main">
+        <div class="row">
+            <div class="box about">
+                <h3 class="boxTitle">
+                    <span class="icon"></span>
+                    <span class="text">درباره من</span>
+                </h3>
+                <div class="boxContent">
+                    <p>
+                        برنامه نویس Full stack طراح وب با بیش از 6 سال کار تخصصی برنامه نویسی.
+                        توسعه دهنده وب با تجربه و مهارت در تمام مراحل توسعه پیشرفته وب. آشنایی کامل در فرایند رابط کاربری، تست و عیب یابی.
+                        متخصص در زمینه طراحی، نصب، تست و نگهداری سیستم‌های وب. دارای مجموعه‌ای متنوع از مهارت‌های برنامه نویسی.
+                        دارای مهارت در زمینه‌های SEO، Backend، Frontend و...
+                        با توانایی انجام پروژه به صورت تکی و همکاری گروهی.
+                    </p>
+                </div>
+            </div>
+            <div class="box skills">
+                <h3 class="boxTitle">
+                    <span class="icon"></span>
+                    <span class="text">مهارت ها</span>
+                </h3>
+                <div class="boxContent">
+                    <div class="skill">PHP</div>
+                    <div class="skill">Laravel</div>
+                    <div class="skill">MySQL</div>
+                    <div class="skill">Java</div>
+                    <div class="skill">HTML</div>
+                    <div class="skill">CSS</div>
+                    <div class="skill">JavaScript</div>
+                    <div class="skill">Vue.js</div>
+                    <div class="skill">React</div>
+                    <div class="skill">jQuery</div>
+                    <div class="skill">UI/UX</div>
+                    <div class="skill">SEO</div>
+                    <div class="skill">Git</div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="box softSkills">
+                <h3 class="boxTitle">
+                    <span class="icon"></span>
+                    <span class="text">ویژگی های فردی</span>
+                </h3>
+                <div class="boxContent">
+                    <div class="softSkillItem">
+                        <strong class="softSkill">روحیه کار تیمی</strong>
+                        <div class="range"><span style="width: 92%;"></span></div>
+                    </div>
+                    <div class="softSkillItem">
+                        <strong class="softSkill">منظم و دقیق</strong>
+                        <div class="range"><span style="width: 88%;"></span></div>
+                    </div>
+                    <div class="softSkillItem">
+                        <strong class="softSkill">خلاقیت</strong>
+                        <div class="range"><span style="width: 80%;"></span></div>
+                    </div>
+                    <div class="softSkillItem">
+                        <strong class="softSkill">مشتاق یادگیری</strong>
+                        <div class="range"><span style="width: 95%;"></span></div>
+                    </div>
+                </div>
+            </div>
+            <div class="box education">
+                <h3 class="boxTitle">
+                    <span class="icon"></span>
+                    <span class="text">سوابق تحصیلی</span>
+                </h3>
+                <div class="boxContent">
+                    <img class="uniLogo" src="img/university.png" alt="دانشگاه آزاد اسلامی"/>
+                    <div class="uniInfo">
+                        <div class="uniInfoItem">کارشناسی مهندسی کامپیوتر</div>
+                        <div class="uniInfoItem">دانشگاه آزاد اسلامی (مشهد)</div>
+                        <div class="uniInfoItem">مهر 1399 - درحال تحصیل</div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+        <div class="box jobs">
+            <h3 class="boxTitle">
+                <span class="icon"></span>
+                <span class="text">سوابق شغلی</span>
+            </h3>
+            <div class="boxContent">
+
+            </div>
+        </div>
     </div>
 </body>
 </html>
