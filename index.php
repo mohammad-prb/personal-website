d85a424addd42a106ff8634ffb9fabe3fc6ec7c7
Mohammad <pspipm27@gmail.com>
2024-07-30 13:56:02 +0330

    start

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fa-ir">
+<head>
+    <title>محمد پوربهزاد - برنامه نویس Full stack</title>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="محمد پوربهزاد,محمد,پوربهزاد,پور بهزاد,محمد پور بهزاد,برنامه نویس,برنامه نویس وب,developer,web developer,frontend,backend,فرانت اند,بک اند,فول استک,mohammad,pourbehzad,mohammad pourbehzad,full stack"/>
+    <meta name="description" content="برنامه نویس Full stack با تجربه، علاقه مند به پروژه ها و کارهای تیمی"/>
+    <meta name="author" content="محمد پوربهزاد"/>
+    <link rel="shortcut icon" href="mohammad.png"/>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body dir="rtl">
+
+</body>
+</html>
