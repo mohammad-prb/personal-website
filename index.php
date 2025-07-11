0997508c552639f64d0ee13d7bfdb8afed43b5a5
Mohammad Pourbehzad <pspipm27@gmail.com>
2025-07-12 02:03:08 +0330

    Fix change theme button.

--- index.php
+++ index.php
@@ -32,9 +32,20 @@ $content = json_decode($file);
 <body dir="<?php echo $lang == 'fa' ? 'rtl' : 'ltr'; ?>" class="<?php echo $lang; ?>">
 <div class="menu">
     <a class="changetThemeBtn" onclick="changeTheme()" href="javascript:void(0);" target="_self">
-        <script>
-            document.write(localStorage.getItem("theme") === "light" ? "" : "");
-        </script>
+        <svg id="lightTheme" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun-icon lucide-sun">
+            <circle cx="12" cy="12" r="4"/>
+            <path d="M12 2v2"/>
+            <path d="M12 20v2"/>
+            <path d="m4.93 4.93 1.41 1.41"/>
+            <path d="m17.66 17.66 1.41 1.41"/>
+            <path d="M2 12h2"/>
+            <path d="M20 12h2"/>
+            <path d="m6.34 17.66-1.41 1.41"/>
+            <path d="m19.07 4.93-1.41 1.41"/>
+        </svg>
+        <svg id="darkTheme" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon-icon lucide-moon">
+            <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/>
+        </svg>
     </a>
     <a class="lang icon" href="<?php echo $lang == 'fa' ? '?en' : ''; ?>" target="_self">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-globe-icon lucide-globe">
@@ -347,4 +358,10 @@ $content = json_decode($file);
 </div>
 </body>
 <script src="js/main.js"></script>
+<script>
+    if (localStorage.getItem("theme") === "light")
+        document.getElementById("darkTheme").classList.add('active');
+    else
+        document.getElementById("lightTheme").classList.add('active');
+</script>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 function changeTheme() {
+    let lmnLight = document.getElementById("lightTheme");
+    let lmnDark = document.getElementById("darkTheme");
+
     if (localStorage.getItem("theme") === "light") {
         localStorage.setItem("theme", "dark");
-        document.querySelector(".changetThemeBtn").innerHTML = "";
+        lmnLight.classList.add('active');
+        lmnDark.classList.remove('active');
     }
     else {
         localStorage.setItem("theme", "light");
-        document.querySelector(".changetThemeBtn").innerHTML = "";
+        lmnLight.classList.remove('active');
+        lmnDark.classList.add('active');
     }
+
     document.querySelector("#theme").href = `css/theme/${localStorage.getItem("theme")}.css?v=${version}`;
 }
 
